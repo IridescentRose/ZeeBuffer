ee3e08d95e94baed489231b6e01f98a38f7fca85
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2025-01-14 19:52:46 -0500

    Add basic C stub

--- src/backend/emitters/CCodegen.zig
+++ src/backend/emitters/CCodegen.zig
@@ -11,9 +11,41 @@ pub fn init(ir: *const IR) Self {
     };
 }
 
-pub fn generate_code(self: *Self, filename: []const u8) !void {
-    _ = self;
-    _ = filename;
+fn generate_states(self: *Self, writer: std.io.AnyWriter) !void {
+    try writer.print("enum ZB_States {{\n", .{});
 
-    std.debug.print("C is not yet supported!\n", .{});
+    var first = true;
+
+    for (self.ir.sym_tab.items) |s| {
+        if (s.type == .State) {
+            if (!first) {
+                try writer.print(",\n", .{});
+            }
+            first = false;
+
+            try writer.print("\tZB_State_{s} = {d}", .{ s.name, s.value });
+        }
+    }
+
+    try writer.print("\n}};\n\n", .{});
+}
+
+pub fn generate_code(self: *Self, filename: []const u8) !void {
+    var file = try std.fs.cwd().createFile(filename, .{});
+    defer file.close();
+
+    const file_writer = file.writer();
+    var buffered_writer = std.io.bufferedWriter(file_writer);
+    defer buffered_writer.flush() catch unreachable;
+
+    const writer = buffered_writer.writer().any();
+
+    try writer.print("#pragma once\n\n", .{});
+
+    try writer.print("enum ZBRole {{\n", .{});
+    try writer.print("\tZB_Role_Server = 0,\n", .{});
+    try writer.print("\tZB_Role_Client = 1\n", .{});
+    try writer.print("}};\n\n", .{});
+
+    try self.generate_states(writer);
 }
--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,7 @@ const Codegen = @import("backend/codegen.zig");
 
 var in_file: ?[]const u8 = null;
 var out_file: ?[]const u8 = null;
+var language: Codegen.TargetLanguage = .Zig;
 
 fn parse_args() !void {
     var arg_it = try std.process.argsWithAllocator(util.allocator());
@@ -19,8 +20,24 @@ fn parse_args() !void {
     out_file = arg_it.next();
 
     if (in_file == null or out_file == null) {
-        std.debug.print("Usage: zbc <in_file> <out_file>\n", .{});
+        std.debug.print("Usage: zbc <in_file> <out_file> [-f {{c, zig}}]\n", .{});
         std.posix.exit(1); // Don't care about cleanup -- we're exiting anyway
+    }
+
+    const format_str = arg_it.next();
+    if (format_str != null and std.mem.eql(u8, format_str.?, "-f")) {
+        const target_lang = arg_it.next();
+
+        if (target_lang) |tl| {
+            if (std.mem.eql(u8, tl, "c")) {
+                language = .C;
+            } else if (std.mem.eql(u8, tl, "zig")) {
+                language = .Zig;
+            } else {
+                std.debug.print("Error: Unrecognized language: {s}!\n", .{tl});
+                std.posix.exit(1);
+            }
+        }
     }
 }
 
@@ -38,5 +55,5 @@ pub fn main() !void {
     var sema = Sema.init(ast, source);
     const ir = try sema.analyze();
 
-    try Codegen.generate_code(&ir, out_file.?, .Zig);
+    try Codegen.generate_code(&ir, out_file.?, language);
 }
